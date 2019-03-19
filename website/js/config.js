ac85a1f9bf1a91dcdc2c56601b5fb85d9abfc256
kmosby <41957086+kmosby@users.noreply.github.com>
2019-03-19 17:55:23 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IRw4g9Wrq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '107sjdu70fqpki8mda20clu4ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_dHKPIhiNd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'hng02sdcbnk1m7i9hdd9o67h7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
